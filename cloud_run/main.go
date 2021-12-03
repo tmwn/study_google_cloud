b3efc22f0a9a7c41ffc88e50c6a3436c87bcd5d7
Your Name <you@example.com>
2021-12-03 23:29:43 +0900

    tmp

--- cloud_run/main.go
+++ cloud_run/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, updated %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "Hello, updated %q!", html.EscapeString(r.URL.Path))
 	})
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
